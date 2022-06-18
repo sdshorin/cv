cbac84ee0567166a382a4c00108f752b29a16331
Shorin Sergey <serres123@ya.ru>
2022-06-19 02:41:29 +0400

    init tex CV

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 
-ls
+
 echo "-----"
 ls working_dir
 echo "-----"
 echo "Endpoint here!"
 
-echo "test CV" > working_dir/CV.pdf
+# echo "test CV" > working_dir/CV.pdf
+
+sudo apt-get install texlive
+
+pdflatex --output-directory=./working_dir CV.tex
+
+
+
+echo "-----"
+ls working_dir
+echo "-----"
+
